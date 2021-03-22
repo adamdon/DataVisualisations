9d1e7a6b174beda1322121dc76b73c951724017c
Adam Don <adamdon89@gmail.com>
2021-03-22 11:44:10 +0000

    Refactor of d3 logic

--- public/pages/visualizations/marketcap/script.js
+++ public/pages/visualizations/marketcap/script.js
@@ -2,10 +2,9 @@ import Data from "./Data.js"
 
 window.onload = async () =>
 {
-    let data = await Data.fetch();
-
-
     let d3 = window.d3;
+
+    let data = await Data.fetch();
 
 
     let duration = 120;
@@ -13,76 +12,54 @@ window.onload = async () =>
     let speed = 10;
 
 
-
-    let names = new Set(data.map(d => d.name));
-    let datevalues = Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
-        .map(([date, data]) => [new Date(date), data])
-        .sort(([a], [b]) => d3.ascending(a, b));
-
-
     let margin = {top: 16, right: 6, bottom: 6, left: 0};
     let barSize = 48;
     let height = (margin.top + barSize * shownBarsNumber + margin.bottom);
     let width = 1000;
+    let colors = getColors();
 
 
-    let x = d3.scaleLinear([0, 1], [margin.left, width - margin.right]);
-    let y = d3.scaleBand().domain(d3.range(shownBarsNumber + 1)).rangeRound([margin.top, margin.top + barSize * (shownBarsNumber + 1 + 0.1)]).padding(0.1);
+    let xHorizontal = d3.scaleLinear([0, 1], [margin.left, width - margin.right]);
+    let yVertical = d3.scaleBand().domain(d3.range(shownBarsNumber + 1)).rangeRound([margin.top, margin.top + barSize * (shownBarsNumber + 1 + 0.1)]).padding(0.1);
 
 
-
-
-    function rank(value)
-    {
-        const data = Array.from(names, name => ({name, value: value(name)}));
-        data.sort((a, b) => d3.descending(a.value, b.value));
-        for (let i = 0; i < data.length; ++i)
-        {
-            data[i].rank = Math.min(shownBarsNumber, i);
-        }
-        return data;
-    }
+    let names = new Set(data.map(d => d.name));
+    let dateValues = getDateValues()
 
 
     let keyframes = getKeyframes();
-    function getKeyframes()
-    {
-        const keyframes = [];
-        let ka, a, kb, b;
-        for ([[ka, a], [kb, b]] of d3.pairs(datevalues))
-        {
-            for (let i = 0; i < speed; ++i)
-            {
-                const t = i / speed;
-                keyframes.push([
-                    new Date(ka * (1 - t) + kb * t),
-                    rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
-                ]);
-            }
-        }
-        keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
-        return keyframes;
-    }
 
+    let nameFrames = getNameFrames();
+    let prevFrames = getPrevFrames();
+    let nextFrames = getNextFrames();
 
-    let nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name);
-    let prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])));
-    let next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
-
+    let formatNumber = d3.format(",d");
     let formatDate = d3.utcFormat("%-d/%-m/%-y");
 
 
-    let color = getColor();
-    function getColor()
+    let barChartSvg = d3.select("#barChartSvg").attr("viewBox", [0, 0, width, height]);
+
+    let updateBars = bars(barChartSvg);
+    let updateAxis = axis(barChartSvg);
+    let updateLabels = labels(barChartSvg);
+    let updateTicker = ticker(barChartSvg);
+
+
+    document.getElementById("loadingP").remove(); //Turn off loading text
+    for (const keyframe of keyframes)
     {
-        const scale = d3.scaleOrdinal(d3.schemeTableau10);
-        if (data.some(d => d.category !== undefined))
-        {
-            const categoryByName = new Map(data.map(d => [d.name, d.category]));
-            scale.domain(categoryByName.values());
-            return d => scale(categoryByName.get(d.name));
-        }
-        return d => scale(d.name);
+        const transition = barChartSvg.transition().duration(duration).ease(d3.easeLinear);
+
+        // Extract the top bar’s value.
+        xHorizontal.domain([0, keyframe[1][0].value]);
+
+        updateAxis(keyframe, transition);
+        updateBars(keyframe, transition);
+        updateLabels(keyframe, transition);
+        updateTicker(keyframe, transition);
+
+        // invalidation.then(() => barChartSvg.interrupt());
+        await transition.end();
     }
 
 
@@ -120,19 +97,19 @@ window.onload = async () =>
             .data(data.slice(0, shownBarsNumber), d => d.name)
             .join(
                 enter => enter.append("rect")
-                    .attr("fill", color)
-                    .attr("height", y.bandwidth())
-                    .attr("x", x(0))
-                    .attr("y", d => y((prev.get(d) || d).rank))
-                    .attr("width", d => x((prev.get(d) || d).value) - x(0)),
+                    .attr("fill", colors)
+                    .attr("height", yVertical.bandwidth())
+                    .attr("x", xHorizontal(0))
+                    .attr("y", d => yVertical((prevFrames.get(d) || d).rank))
+                    .attr("width", d => xHorizontal((prevFrames.get(d) || d).value) - xHorizontal(0)),
                 update => update,
                 exit => exit.transition(transition).remove()
-                    .attr("y", d => y((next.get(d) || d).rank))
-                    .attr("width", d => x((next.get(d) || d).value) - x(0))
+                    .attr("y", d => yVertical((nextFrames.get(d) || d).rank))
+                    .attr("width", d => xHorizontal((nextFrames.get(d) || d).value) - xHorizontal(0))
             )
             .call(bar => bar.transition(transition)
-                .attr("y", d => y(d.rank))
-                .attr("width", d => x(d.value) - x(0)));
+                .attr("y", d => yVertical(d.rank))
+                .attr("width", d => xHorizontal(d.value) - xHorizontal(0)));
     }
 
 
@@ -141,10 +118,10 @@ window.onload = async () =>
         const g = svg.append("g")
             .attr("transform", `translate(0,${margin.top})`);
 
-        const axis = d3.axisTop(x)
+        const axis = d3.axisTop(xHorizontal)
             .ticks(width / 160)
             .tickSizeOuter(0)
-            .tickSizeInner(-barSize * (shownBarsNumber + y.padding()));
+            .tickSizeInner(-barSize * (shownBarsNumber + yVertical.padding()));
 
         return (_, transition) =>
         {
@@ -156,16 +133,9 @@ window.onload = async () =>
     }
 
 
-    let formatNumber = d3.format(",d");
 
-    function textTween(a, b)
-    {
-        const i = d3.interpolateNumber(a, b);
-        return function (t)
-        {
-            this.textContent = formatNumber(i(t));
-        };
-    }
+
+
 
 
 
@@ -181,8 +151,8 @@ window.onload = async () =>
             .data(data.slice(0, shownBarsNumber), d => d.name)
             .join(
                 enter => enter.append("text")
-                    .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
-                    .attr("y", y.bandwidth() / 2)
+                    .attr("transform", d => `translate(${xHorizontal((prevFrames.get(d) || d).value)},${yVertical((prevFrames.get(d) || d).rank)})`)
+                    .attr("y", yVertical.bandwidth() / 2)
                     .attr("x", -6)
                     .attr("dy", "-0.25em")
                     .text(d => d.name)
@@ -193,44 +163,115 @@ window.onload = async () =>
                         .attr("dy", "1.15em")),
                 update => update,
                 exit => exit.transition(transition).remove()
-                    .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
-                    .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
+                    .attr("transform", d => `translate(${xHorizontal((nextFrames.get(d) || d).value)},${yVertical((nextFrames.get(d) || d).rank)})`)
+                    .call(g => g.select("tspan").tween("text", d => textTween(d.value, (nextFrames.get(d) || d).value)))
             )
             .call(bar => bar.transition(transition)
-                .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
-                .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
+                .attr("transform", d => `translate(${xHorizontal(d.value)},${yVertical(d.rank)})`)
+                .call(g => g.select("tspan").tween("text", d => textTween((prevFrames.get(d) || d).value, d.value))));
     }
 
 
-    //Turn off loading text
-    const loadingPElement = document.getElementById("loadingP");
-    loadingPElement.remove();
-
-    const svg = d3.select("#barChart").attr("viewBox", [0, 0, width, height]);
-
-
-    const updateBars = bars(svg);
-    const updateAxis = axis(svg);
-    const updateLabels = labels(svg);
-    const updateTicker = ticker(svg);
 
 
 
-    for (const keyframe of keyframes)
+
+
+
+    ///////////////////////////////////////////////////////////////////
+    //////////////////////// multi call functions///////////////////////
+    ///////////////////////////////////////////////////////////////////
+
+
+    function rank(value)
     {
-        const transition = svg.transition().duration(duration).ease(d3.easeLinear);
-
-        // Extract the top bar’s value.
-        x.domain([0, keyframe[1][0].value]);
-
-        updateAxis(keyframe, transition);
-        updateBars(keyframe, transition);
-        updateLabels(keyframe, transition);
-        updateTicker(keyframe, transition);
-
-        // invalidation.then(() => svg.interrupt());
-        await transition.end();
+        const data = Array.from(names, name => ({name, value: value(name)}));
+        data.sort((a, b) => d3.descending(a.value, b.value));
+        for (let i = 0; i < data.length; ++i)
+        {
+            data[i].rank = Math.min(shownBarsNumber, i);
+        }
+        return data;
     }
 
+
+    function textTween(a, b)
+    {
+        const i = d3.interpolateNumber(a, b);
+        return function (t)
+        {
+            this.textContent = formatNumber(i(t));
+        };
+    }
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////
+    //////////////////////// single call getters///////////////////////
+    ///////////////////////////////////////////////////////////////////
+
+
+    function getDateValues()
+    {
+        return Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
+            .map(([date, data]) => [new Date(date), data])
+            .sort(([a], [b]) => d3.ascending(a, b));
+    }
+
+
+
+
+    function getNameFrames()
+    {
+        return d3.groups(keyframes.flatMap(([, data]) => data), d => d.name);
+    }
+
+
+    function getPrevFrames()
+    {
+        return new Map(nameFrames.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])));
+    }
+
+    function getNextFrames()
+    {
+        return new Map(nameFrames.flatMap(([, data]) => d3.pairs(data)));
+    }
+
+
+    function getKeyframes()
+    {
+        const keyframes = [];
+        let ka, a, kb, b;
+        for ([[ka, a], [kb, b]] of d3.pairs(dateValues))
+        {
+            for (let i = 0; i < speed; ++i)
+            {
+                const t = i / speed;
+                keyframes.push([
+                    new Date(ka * (1 - t) + kb * t),
+                    rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
+                ]);
+            }
+        }
+        keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
+        return keyframes;
+    }
+
+
+
+
+    function getColors()
+    {
+        const scale = d3.scaleOrdinal(d3.schemeTableau10);
+        if (data.some(d => d.category !== undefined))
+        {
+            const categoryByName = new Map(data.map(d => [d.name, d.category]));
+            scale.domain(categoryByName.values());
+            return d => scale(categoryByName.get(d.name));
+        }
+        return d => scale(d.name);
+    }
 
 }
