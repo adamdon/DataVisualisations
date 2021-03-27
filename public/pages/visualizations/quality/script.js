661a558fbcbaf00862b36f6ea928dbfa83bbcc87
sir-grr <grr1206@gmail.com>
2021-03-27 16:52:24 +0000

    quality drop down selection working

--- public/pages/visualizations/quality/script.js
+++ public/pages/visualizations/quality/script.js
@@ -6,10 +6,10 @@ window.onload = function () {
     
     var countrySelection = document.getElementById("countryDropDown");
     var countrySlug = countrySelection.value;
+    var countryName = countrySelection.options[countrySelection.selectedIndex].text;
     console.log(countrySlug)
 
     const url = `https://api.teleport.org/api/urban_areas/slug:${countrySlug}/scores/`
-    console.log(url);
     const response = await fetch(url)
     const data = await response.json() 
     console.log(data)
@@ -19,7 +19,7 @@ window.onload = function () {
     animationEnabled: true,
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     title:{
-        text: "San Fransisco"
+        text: countryName
     },
     axisY: {
         title: "Score Out Of Ten"
@@ -36,17 +36,18 @@ window.onload = function () {
 
     data.categories.forEach(category => {
         chart.options.data[0].dataPoints.push({ y: category.score_out_of_10, label: category.name, color: category.color})
-        console.log(category)
+        //console.log(category)
     });
 
     chart.render();
     }
     
-    document.getElementById('countryDropDown').addEventListener('change',runFunction)
+    document.getElementById('countryDropDown').addEventListener('change',rungetData)
 
-    function runFunction(){
-        console.log("hello");
+    function rungetData(){
         getData();
     }
+
+    getData();
 
 }
