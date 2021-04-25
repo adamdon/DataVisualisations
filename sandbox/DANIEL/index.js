0529a710f5c868c4eadd94d33ae1631dc7477464
Adam Don <adamdon89@gmail.com>
2021-04-25 19:16:57 +0100

    Removed sandbox testing directory

--- sandbox/DANIEL/index.js
+++
@@ -1,7 +0,0 @@
-const userInput = prompt ("Enter something")
-
-if(userInput){
-    console.log("Truthy!")
-}else{
-    console.log("Falsy!")
-}
--- sandbox/duncan/frontend/main.js
+++
@@ -1,4 +0,0 @@
-import App from './App.js';
-
-const app = Vue.createApp({render: () => Vue.h(App)});
-app.mount('#App');
